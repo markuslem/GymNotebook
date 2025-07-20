642894a5ebed927981591f9f017f143dce8038ba
markuslem <markus.lemberg@ut.ee>
2025-07-20 17:38:31 +0300

    other tables (no relations yet)

--- app/src/main/java/com/example/gymnotebook/data/DataEntities.kt
+++ app/src/main/java/com/example/gymnotebook/data/DataEntities.kt
@@ -2,18 +2,55 @@ package com.example.gymnotebook.data
 
 
 import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Relation
 import androidx.room.TypeConverters
 import java.util.Date
 import java.util.UUID
 
 @Entity(tableName = "workouts")
 data class WorkoutEntity(
-    @PrimaryKey val workoutId: Int,
+    @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "start_date") val startDate: Date,
     @ColumnInfo(name = "end_date") var endDate: Date?,
     @ColumnInfo(name = "total_weight") var totalWeight: Int,
-//    @ColumnInfo(name = "exercises") var exercises: List<Exercise>
+)
+
+/* Dataclass for the relationship between WorkoutEntity and ExerciseEntity */
+data class WorkoutWithExercises(
+    @Embedded
+    val workout: WorkoutEntity,
+
+    @Relation(
+        parentColumn = "workoutId",    // Primary key of the WorkoutEntity
+        entityColumn = "workoutOwnerId", // Foreign key in the ExerciseEntity
+        entity = ExerciseEntity::class // Child entity
+    )
+    val exercises: List<ExerciseEntity>
+)
+
+@Entity(tableName = "exercises",
+    )
+data class ExerciseEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "desc") val desc: ExerciseDesc,
+    @ColumnInfo(name = "sets") var sets: List<SetOfExercise>
+)
+
+@Entity(tableName = "set")
+data class SetEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "reps") var reps: Int,
+    @ColumnInfo(name = "weight") var weight: Float,
+    @ColumnInfo(name = "done") var done: Boolean
+)
+
+@Entity(tableName = "exercise_desc")
+data class ExerciseDescEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "name") val name: String,
+    // And more...
 )
